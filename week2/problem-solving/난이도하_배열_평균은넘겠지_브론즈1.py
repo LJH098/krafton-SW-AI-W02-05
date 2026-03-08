47b9cf102144ec3bbd9c015afa3e7c244c291bb9
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-08 20:54:10 +0900

    solve: 배열_평균은넘겠지

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,23 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+from sys import stdin
+
+TEST_CASE = int(stdin.readline())
+
+for i in range(TEST_CASE):
+    student_info = list(map(int, stdin.readline().split()))
+    sum = 0
+    over = 0
+    sl = len(student_info)
+    for j in range(1, sl):
+        sum += student_info[j]
+    avg = sum / (sl - 1)
+
+    for j in range(1, sl):
+        if student_info[j] > avg:
+            over += 1
+
+    ratio = (over / (sl - 1)) * 100
+    format_ratio = round(ratio, 3)
+    print(f"{format_ratio}%")
