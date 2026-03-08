a8db636e419d5b764f9eead599a7b7a41981ccd1
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-08 20:53:57 +0900

    solve: 문자열_문자열반복

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+from sys import stdin
+
+TEST_CASE = int(stdin.readline())
+
+for i in range(TEST_CASE):
+    k, m = stdin.readline().split()
+    list_m = list(m)
+    n = int(k)
+    answer = ""
+
+    for j in range(len(list_m)):
+        word = list_m[j] * n
+
+        answer += word
+    print(answer)
