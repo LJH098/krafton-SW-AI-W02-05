46245c3bbaecd1272210597f11e198dc56c1730e
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-08 20:54:41 +0900

    solve: 파이썬문법_최댓값

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,22 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+from sys import stdin
+
+list = []
+for i in range(9):
+    n = int(stdin.readline())
+    list.append(n)
+
+max = 0
+line = 0
+for i in range(9):
+    if i == 0:
+        max = list[0]
+        line = 1
+    elif list[i] > max:
+        max = list[i]
+        line = i + 1
+
+print(max)
+print(line)
