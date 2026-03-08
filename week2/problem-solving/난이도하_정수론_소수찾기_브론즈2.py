fe1ca1f5e3a282ab5585544b4e84b70d8d629cc4
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-08 20:54:29 +0900

    solve: 정수론_소수찾기

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,28 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+from sys import stdin
+import math
+
+
+def is_prime(num):
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    elif num % 2 == 0:
+        return False
+
+    sqrt = math.trunc(math.sqrt(num))
+
+    for i in range(3, sqrt + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+
+
+n = int(stdin.readline())
+nums = list(map(int, stdin.readline().split()))
+
+answers = [i for i in nums if is_prime(i)]
+print(len(answers))
