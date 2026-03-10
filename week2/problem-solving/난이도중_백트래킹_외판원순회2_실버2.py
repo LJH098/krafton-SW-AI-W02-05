237720717d3a8c329027ccd4b66f3249aaecf1b0
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-10 23:02:28 +0900

    solve: 외판원순회2

--- week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
+++ week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
@@ -1,2 +1,30 @@
 # 백트래킹 - 외판원 순회 2 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10971
+from sys import stdin
+
+n = int(stdin.readline())
+w = [list(map(int, stdin.readline().split())) for _ in range(n)]
+
+lowest = float("inf")
+
+
+def tsp(start, current, visited, cost):
+    global lowest
+
+    if cost >= lowest:
+        return
+
+    if len(visited) == n:
+        if w[current][start] != 0:
+            lowest = min(lowest, cost + w[current][start])
+            return
+
+    for next_city in range(n):
+        if next_city not in visited and w[current][next_city] != 0:
+            visited.append(next_city)
+            tsp(start, next_city, visited, cost + w[current][next_city])
+            visited.pop()
+
+
+tsp(0, 0, [0], 0)
+print(lowest)
