02c847215660476d398046080f03095380e8a513
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-10 23:02:07 +0900

    solve: 골드바흐의추측

--- week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
+++ week2/problem-solving/난이도중_정수론_골드바흐의추측_실버2.py
@@ -1,2 +1,21 @@
-# 정수론 - 골드바흐의 추측 (백준 실버2)
-# 문제 링크: https://www.acmicpc.net/problem/9020
+from sys import stdin
+import math
+
+n = int(stdin.readline())
+arr = [int(stdin.readline()) for _ in range(n)]
+
+# 에라토스테네스의 체로 소수 미리 계산
+MAX = 10001
+sieve = [True] * MAX
+sieve[0] = sieve[1] = False
+
+for i in range(2, int(math.sqrt(MAX)) + 1):
+    if sieve[i]:
+        for j in range(i * i, MAX, i):
+            sieve[j] = False
+
+for num in arr:
+    for i in range(num // 2, 1, -1):
+        if sieve[i] and sieve[num - i]:
+            print(f"{i} {num-i}")
+            break
