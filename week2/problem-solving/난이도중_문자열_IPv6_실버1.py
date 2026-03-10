7270e3a570cd9481d85e0fdb64616c75073bb13b
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-10 23:02:19 +0900

    solve: 문자열_IPV6

--- week2/problem-solving/난이도중_문자열_IPv6_실버1.py
+++ week2/problem-solving/난이도중_문자열_IPv6_실버1.py
@@ -1,2 +1,29 @@
 # 문자열 - IPv6 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/3107
+
+from sys import stdin
+
+
+# list에서 빈칸이 연속해서 2개가 있는지 확인
+# 참일 때는 (True, index) 거짓일 때는 (False,0)을 반환
+address = stdin.readline().strip()
+full_address = []
+
+
+def fill(word):
+    return word.zfill(4)
+
+
+# ::을 나누면 무조건 두개로 나뉘어짐  (::을 한번만 사용 할 수 있기 때문)
+if "::" in address:
+    left, right = address.split("::")
+    left_group = [fill(g) for g in left.split(":") if g]
+    right_group = [fill(g) for g in right.split(":") if g]
+
+    count = 8 - len(left_group) - len(right_group)
+    zero_group = ["0000"] * count
+    full_address = left_group + zero_group + right_group
+else:
+    full_address = [fill(g) for g in address.split(":") if g]
+
+print(":".join(full_address))
