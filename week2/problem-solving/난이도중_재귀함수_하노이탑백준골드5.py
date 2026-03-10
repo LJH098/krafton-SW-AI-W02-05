93fe68b01603e0ddae3457b4c47847b863a13201
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-10 23:06:28 +0900

    solve: 하노이탑

--- week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,30 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+from sys import stdin
+
+num = int(stdin.readline())
+
+count = 0
+
+
+def hanoi(n, start, des, via):
+    global count
+    if n == 1:
+        print(f"{start} {des}")
+        count += 1
+        return
+
+    # n-1개를 경유지로 옮기기
+    hanoi(n - 1, start, via, des)
+
+    # 밑바닥 목적지로 옮기기
+    hanoi(1, start, des, via)
+
+    # 경유지로 옮긴 것 목적지로 옮기기
+    hanoi(n - 1, via, des, start)
+
+
+print(2**num - 1)
+if num <= 20:
+    hanoi(num, 1, 3, 2)
