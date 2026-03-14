cb5eab055a670229e8cf723f71728d3be17cb7b4
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-14 16:23:02 +0900

    solve: 큐_카드2

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,20 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from sys import stdin
+from collections import deque
+
+card_queue = deque()
+drop_queue = []
+n = int(stdin.readline())
+
+# 4,3,2,1 -> 4,3,2 drop = 1 -> 2,4,3 drop =1
+for i in range(1, n + 1):
+    card_queue.append(i)
+
+while len(card_queue) > 1:
+    card_queue.popleft()  # 버리기
+    card = card_queue.popleft()
+    card_queue.append(card)
+
+print(card_queue[0])
