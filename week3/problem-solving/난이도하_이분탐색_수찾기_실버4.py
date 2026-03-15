c957f07314e158d7166b860b6f4d9c29bbc58f9a
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-15 17:42:46 +0900

    solve: 수찾기

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,18 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+from sys import stdin
+
+readline = stdin.readline
+n = int(readline())
+num_list = list(map(int, readline().split()))
+dic = {}
+for i in range(n):
+    dic[num_list[i]] = 0
+
+n2 = int(readline())
+num2_list = list(map(int, readline().split()))
+for i in range(n2):
+    if num2_list[i] in dic.keys():
+        print("1")
+    else:
+        print("0")
