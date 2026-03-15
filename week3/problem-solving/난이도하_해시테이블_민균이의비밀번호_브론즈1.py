38a2e51f21c10c64141f1eb22475399088482436
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-15 17:42:57 +0900

    solve: 민균이의비밀번호

--- week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
+++ week3/problem-solving/난이도하_해시테이블_민균이의비밀번호_브론즈1.py
@@ -1,2 +1,18 @@
 # 해시 테이블 - 민균이의 비밀번호 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/9933
+from sys import stdin
+
+n = int(stdin.readline())
+pwd = {}
+
+for i in range(n):
+    pwd[stdin.readline().strip()] = 0
+
+
+for word in pwd.keys():
+    reversed_word = word[::-1]
+    if reversed_word in pwd:
+        length = len(word)
+        half = (length - 1) // 2
+        print(f"{length} {word[half]}")
+        break
