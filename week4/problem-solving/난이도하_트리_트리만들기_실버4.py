39e4e2c9b8124e6f3792d2038a3cc18848468184
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-23 13:24:02 +0900

    solve: 트리만들기

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,15 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+from sys import stdin
+
+input = stdin.readline
+
+n, m = map(int, input().split())
+
+
+cur = 0
+for next in range(1, n):
+    print(cur, next)
+    if cur < n - m:
+        cur += 1
