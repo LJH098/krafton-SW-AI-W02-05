6bbc98e78c43d0a0db0b766f63af92ae7b4c919c
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-25 00:27:45 +0900

    solve: 중 문제 풀이

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,35 @@
-# BFS - 동전 2 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/2294
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+coins = []
+for _ in range(n):
+    coin = int(input())
+    coins.append(coin)
+
+
+INF = 10**9
+
+
+dp = [INF] * (m + 1)
+
+
+dp[0] = 0
+
+
+for coin in coins:
+
+    for x in range(coin, m + 1):
+        # TODO:
+        # x - coin원을 만들 수 있었다면
+        # dp[x]를 더 작은 값으로 갱신
+        if x - coin >= 0:
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+
+if dp[m] == INF:
+    print(-1)
+else:
+    print(dp[m])
--- week4/problem-solving/난이도중_BFS_미로탐색_실버1.py
+++ week4/problem-solving/난이도중_BFS_미로탐색_실버1.py
@@ -1,2 +1,33 @@
 # BFS - 미로 탐색 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/2178
+
+# 최소 칸 -> bfs
+from sys import stdin
+from collections import deque
+
+input = stdin.readline
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().strip())) for _ in range(n)]
+
+queue = deque([(0, 0)])
+visited = [[False] * m for _ in range(n)]
+visited[0][0] = True
+dist = [[0] * m for _ in range(n)]
+dist[0][0] = 1
+while queue:
+    r, c = queue.popleft()
+    if r == n - 1 and c == m - 1:
+        print(dist[r][c])
+        break
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    for dr, dc in directions:
+        nr = r + dr
+        nc = c + dc
+
+        if 0 <= nr < n and 0 <= nc < m and not visited[nr][nc] and arr[nr][nc] == 1:
+            visited[nr][nc] = True
+            dist[nr][nc] = dist[r][c] + 1
+            queue.append((nr, nc))
