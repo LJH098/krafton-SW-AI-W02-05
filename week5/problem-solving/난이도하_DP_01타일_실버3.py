3d3869fba986ab1aef9528a792d731e33ccec929
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-27 21:17:07 +0900

    solve: 01타일

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,19 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+import sys
+
+
+input = sys.stdin.readline
+
+n = int(input())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    a, b = 1, 2
+    for _ in range(3, n + 1):
+        a, b = b, (a + b) % 15746
+    print(b)
