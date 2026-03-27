9f1eb7f4132329bf6282d4242a9dc4c7d03f449e
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-27 15:48:03 +0900

    solve: 피보나치수2

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,25 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input())
+memory = {}
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    if n in memory:
+        return memory[n]
+
+    memory[n] = fib(n - 1) + fib(n - 2)
+    return memory[n]
+
+
+print(fib(n))
