819c322520526eadd7fe43c7debf160ed0194ed8
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-27 21:17:17 +0900

    solve: 동전0

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,18 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+
+count = 0
+for i in range(len(arr) - 1, -1, -1):
+    if arr[i] > m:
+        continue
+    n = m // arr[i]
+    m %= arr[i]
+    count += n
+print(count)
