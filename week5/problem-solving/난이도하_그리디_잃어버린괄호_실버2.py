394fdc10e66e249a2d1c02d1d147a2cd80e8f67a
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-27 21:17:29 +0900

    solve: 잃어버린괄호

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,20 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+import sys
+
+input = sys.stdin.readline
+total = 0
+arr = input().split("-")
+for i in range(len(arr)):
+    if i == 0:
+        if "+" in arr[i]:
+            total += sum(int(num) for num in arr[i].split("+"))
+        else:
+            total += int(arr[i])
+    else:
+        if "+" in arr[i]:
+            total -= sum(int(num) for num in arr[i].split("+"))
+        else:
+            total -= int(arr[i])
+print(total)
