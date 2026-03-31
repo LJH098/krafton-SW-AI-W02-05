e2cac20439463e0dc3b870b310f0cceea1ca43e9
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-31 10:17:13 +0900

    solve: 동전

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,19 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+import sys
+
+input = sys.stdin.readline
+
+TEST_CASE = int(input())
+for _ in range(TEST_CASE):
+    n = int(input())  # n = 동전 가지수
+    arr = list(map(int, input().split()))
+    m = int(input())  # m = 만들어야 할 금액
+    dp = [0] * (m + 1)
+    dp[0] = 1
+
+    for coin in arr:
+        for i in range(coin, m + 1):
+            dp[i] += dp[i - coin]
+    print(dp[m])
