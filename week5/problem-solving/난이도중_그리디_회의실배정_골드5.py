53d924168423112ab8804db78898e860ed24b9ee
Jin Hyuk <ekrrdj07@gmail.com>
2026-03-31 10:17:04 +0900

    solve: 회의실 배정

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,24 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = [tuple(map(int, input().split())) for _ in range(n)]
+if len(arr) == 1:
+    print(1)
+    sys.exit()
+
+arr.sort(key=lambda a: (a[1], a[0]))
+
+select = arr[0]
+count = 1
+
+for a, b in arr[1:]:
+    if a >= select[1]:
+        count += 1
+        select = (a, b)
+print(count)
